10eda4be593a4913a6dd88e5c7dd7a92e90ad7be
Radheshyam Tiwari <radhe.tiwari@ibm.com>
2025-12-24 15:32:04 +0530

    updated terraform dev variable

--- greetflow-poc/infra/terraform/envs/dev/variables.tf
+++ greetflow-poc/infra/terraform/envs/dev/variables.tf
@@ -1,5 +1,22 @@
-variable "region" { type = string default = "ap-south-1" }
-variable "domain_name" { type = string default = "rdhcloudlab.com" }
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
 
-variable "cluster_name" { type = string default = "greetflow-dev-eks" }
-variable "tags" { type = map(string) default = { project = "greetflow", env = "dev" } }
+variable "domain_name" {
+  type    = string
+  default = "rdhcloudlab.com"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "greetflow-dev-eks"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    project = "greetflow"
+    env     = "dev"
+  }
+}
--- greetflow-poc/infra/terraform/envs/prod/variables.tf
+++ greetflow-poc/infra/terraform/envs/prod/variables.tf
@@ -1,5 +1,22 @@
-variable "region" { type = string default = "ap-south-1" }
-variable "domain_name" { type = string default = "rdhcloudlab.com" }
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
 
-variable "cluster_name" { type = string default = "greetflow-prod-eks" }
-variable "tags" { type = map(string) default = { project = "greetflow", env = "prod" } }
+variable "domain_name" {
+  type    = string
+  default = "rdhcloudlab.com"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "greetflow-prod-eks"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    project = "greetflow"
+    env     = "prod"
+  }
+}
