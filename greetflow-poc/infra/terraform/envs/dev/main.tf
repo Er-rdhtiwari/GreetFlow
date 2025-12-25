c7a5554eb87438fcb61248bd7cd44d875245661b
rdh tiwari <tiwari.rdhtiwari@gmail.com>
2025-12-25 12:30:09 +0000

    updated from ec2

--- greetflow-poc/infra/terraform/envs/dev/main.tf
+++ greetflow-poc/infra/terraform/envs/dev/main.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.80" }
+    aws        = { source = "hashicorp/aws", version = "~> 5.80" }
     kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.35" }
-    helm = { source = "hashicorp/helm", version = "~> 2.16" }
+    helm       = { source = "hashicorp/helm", version = "~> 2.16" }
   }
 }
 
@@ -17,31 +17,31 @@ data "aws_route53_zone" "root" {
 }
 
 module "vpc" {
-  source = "../../modules/vpc"
-  name_prefix = "greetflow-dev"
-  cidr = "10.20.0.0/16"
-  azs  = ["ap-south-1a", "ap-south-1b"]
+  source          = "../../modules/vpc"
+  name_prefix     = "greetflow-dev"
+  cidr            = "10.20.0.0/16"
+  azs             = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.20.1.0/24", "10.20.2.0/24"]
   private_subnets = ["10.20.11.0/24", "10.20.12.0/24"]
-  tags = var.tags
+  tags            = var.tags
 }
 
 module "eks" {
-  source = "../../modules/eks"
-  cluster_name = var.cluster_name
-  vpc_id = module.vpc.vpc_id
+  source             = "../../modules/eks"
+  cluster_name       = var.cluster_name
+  vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
-  tags = var.tags
+  tags               = var.tags
 }
 
 # Data sources wait for cluster
 data "aws_eks_cluster" "this" {
   depends_on = [module.eks]
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
 }
 data "aws_eks_cluster_auth" "this" {
   depends_on = [module.eks]
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
 }
 
 provider "kubernetes" {
@@ -64,28 +64,28 @@ module "ecr" {
 }
 
 module "acm" {
-  source = "../../modules/acm"
-  create = true
+  source      = "../../modules/acm"
+  create      = true
   domain_name = var.domain_name
-  zone_id = data.aws_route53_zone.root.zone_id
+  zone_id     = data.aws_route53_zone.root.zone_id
 }
 
 module "iam_irsa" {
-  source = "../../modules/iam_irsa"
-  cluster_name = module.eks.cluster_name
+  source            = "../../modules/iam_irsa"
+  cluster_name      = module.eks.cluster_name
   oidc_provider_arn = module.eks.oidc_provider_arn
-  tags = var.tags
+  tags              = var.tags
 }
 
 module "addons" {
-  source = "../../modules/addons"
-  cluster_name = module.eks.cluster_name
-  region = var.region
-  vpc_id  = module.vpc.vpc_id
+  source        = "../../modules/addons"
+  cluster_name  = module.eks.cluster_name
+  region        = var.region
+  vpc_id        = module.vpc.vpc_id
   domain_filter = var.domain_name
 
-  alb_role_arn = module.iam_irsa.alb_role_arn
-  external_dns_role_arn = module.iam_irsa.external_dns_role_arn
+  alb_role_arn              = module.iam_irsa.alb_role_arn
+  external_dns_role_arn     = module.iam_irsa.external_dns_role_arn
   external_secrets_role_arn = module.iam_irsa.external_secrets_role_arn
 }
 # Download the official policy JSON and keep it in repo (next step)
--- greetflow-poc/infra/terraform/envs/dev/outputs.tf
+++ greetflow-poc/infra/terraform/envs/dev/outputs.tf
@@ -1,6 +1,6 @@
 output "cluster_name" { value = module.eks.cluster_name }
 output "cluster_endpoint" { value = module.eks.cluster_endpoint }
 output "ecr_api_repo_url" { value = module.ecr.api_repo_url }
-output "ecr_ui_repo_url"  { value = module.ecr.ui_repo_url }
-output "acm_cert_arn"     { value = module.acm.cert_arn }
-output "hosted_zone_id"   { value = data.aws_route53_zone.root.zone_id }
+output "ecr_ui_repo_url" { value = module.ecr.ui_repo_url }
+output "acm_cert_arn" { value = module.acm.cert_arn }
+output "hosted_zone_id" { value = data.aws_route53_zone.root.zone_id }
